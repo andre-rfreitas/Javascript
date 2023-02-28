5e643300f2f4cd94282717ad89c28f80219a0e6b
André Freitas <andre.rfreitass@gmail.com>
2023-02-28 15:09:42 -0300

    FOR

---
+++ JavaScript-modulo-01/aula-28/for.js
@@ -0,0 +1,19 @@
+/*
+alert("1")
+alert("2")
+alert("3")
+alert("4")
+alert("5")
+alert("6")
+alert("7")
+*/
+// inicializaçao; condicao; finalizacao
+// for (let indice = 0; indice <= 10; indice++) {
+//     alert("Indice = " + indice)
+// }
+
+let nome = "André Ribeiro"
+
+for (let indice = 1; indice < nome.length; indice++) {
+    console.log(nome[indice])
+}
