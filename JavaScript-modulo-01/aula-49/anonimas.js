eab1840d7e26f37ef828aa8ea489008789e2523e
André Freitas <andre.rfreitass@gmail.com>
2023-03-03 11:20:54 -0300

    funções anônimas

---
+++ JavaScript-modulo-01/aula-49/anonimas.js
@@ -0,0 +1,24 @@
+function somar(a, b) {
+    return a + b
+}
+
+let operacao = somar
+
+console.log(operacao(4, 5))
+
+operacao = function (a, b) {
+    return a - b
+}
+
+console.log(operacao(4, 5))
+
+olaMundo()
+oiMundo()
+
+function olaMundo() {
+    console.log("Olá, mundo!")
+}
+
+const oiMundo = function() {
+    console.log("Oi, mundo!")
+}
