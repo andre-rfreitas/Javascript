c821676eff700dc0fcc4b619b9170e2586980ee7
André Freitas <andre.rfreitass@gmail.com>
2023-03-08 11:21:19 -0300

    tipos de dados

---
+++ B7/Tipos-de-dados/script.js
@@ -0,0 +1,12 @@
+let nome = "André"
+let idade = 90
+let logado = true //false
+let lista = ["ovo", "massa", "corante"]
+let nomeCompleto = { nome: "André", idade: 90 }
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof nomeCompleto)
+
