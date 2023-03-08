06b5b5c43c2be52c5c0c44ceada423b0b467f7a5
André Freitas <andre.rfreitass@gmail.com>
2023-03-08 11:47:27 -0300

    template string

---
+++ B7/String e Template String/script.js
@@ -0,0 +1,15 @@
+let nome = "André"
+let sobrenome = "Freitas"
+
+//let nomeCompleto = nome + ' ' + sobrenome
+let nomeCompleto = `${nome} ${sobrenome}`
+
+console.log("Nome: " + nomeCompleto)
+
+
+
+let idade = 22
+
+let idadeString = `Idade: ${idade + 2} anos`
+
+console.log(idadeString)
