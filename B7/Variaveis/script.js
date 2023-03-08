489d6d696df1dbd4f3608be5ddd66fa9627f2560
André Freitas <andre.rfreitass@gmail.com>
2023-03-08 11:38:54 -0300

    operações em variaveis

--- B7/Variaveis/script.js
+++ B7/Variaveis/script.js
@@ -3,3 +3,12 @@ let name = "José"
 
 
 console.log(`${name} tem ${age} anos`)
+
+// operações em variaveis
+let x = 1
+x = x + 1 - 6 * 4 / 9
+console.log(x)
+
+let y = "2"
+y = y + 8
+console.log(y)
