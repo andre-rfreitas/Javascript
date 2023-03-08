395a7424f017c294daf445d45c775b5d1a45dda0
André Freitas <andre.rfreitass@gmail.com>
2023-03-08 11:30:11 -0300

    exercicio 1 variaveis

---
+++ B7/Variaveis/exercicio-1/exe.js
@@ -0,0 +1,15 @@
+// 1. Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. E exiba.
+let car = "Ferrari"
+
+console.log(car)
+
+// 2. Crie uma variável para armazenar o preço do bolo que custa R$ 19,99. E exiba.
+let cakePrice = 19.99
+
+console.log(cakePrice)
+
+// 3. Corrija o código a seguir (e exiba).
+// let "cidade" = São Paulo
+let cidade = "São Paulo"
+
+console.log(cidade)
