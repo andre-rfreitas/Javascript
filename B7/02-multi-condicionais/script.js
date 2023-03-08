98a3759b16c007b94546c53d101d6c40027e0771
André Freitas <andre.rfreitass@gmail.com>
2023-03-08 12:05:52 -0300

    condicional &&

---
+++ B7/02-multi-condicionais/script.js
@@ -0,0 +1,12 @@
+let idade = 45
+/*
+if (idade >= 18) {
+    if(idade < 60) {
+        console.log("Você é um adulto")
+    }
+}
+*/
+
+if (idade >= 18 && idade < 60) {
+    console.log("Você é um adulto.")
+}
