69ad03f3788e5914f21bc95fdb5908981106b6a3
André Freitas <andre.rfreitass@gmail.com>
2023-03-09 09:47:02 -0300

    condicional ternario

---
+++ B7/3-condicional-ternario/script.js
@@ -0,0 +1,13 @@
+let isMember = false // true
+
+let shipping = isMember ? 2 : 10 
+
+console.log(isMember ? "Você é mebro" : "Você não é membro")
+console.log("Frete: " + shipping)
+
+
+let age = 10
+
+let isAdult = (age >= 18 ? "Sim" : "Não")
+
+console.log(isAdult)
