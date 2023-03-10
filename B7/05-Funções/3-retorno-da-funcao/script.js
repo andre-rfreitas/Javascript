ccc74007efea6f930fee7d2c1bd4cb146e8b9340
André Freitas <andre.rfreitass@gmail.com>
2023-03-10 20:49:07 -0300

    retorno da funcao

---
+++ B7/05-Funções/3-retorno-da-funcao/script.js
@@ -0,0 +1,10 @@
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`
+}
+
+let completo = nomeCompleto("André", "Freitas")
+console.log(completo)
+
+console.log(nomeCompleto("André", "Freitas"))
+
+// ENTRADA -> PROCESSAMENTO -> SAÍDA
