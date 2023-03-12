54875d677474b6284655b70d5f140941e329dfdb
André Freitas <andre.rfreitass@gmail.com>
2023-03-12 11:29:47 -0300

    intro arrays

---
+++ B7/06-Arrays/01-intro/script.js
@@ -0,0 +1,10 @@
+let colors = ['blue', 'red', 'green']
+console.log(colors)
+console.log(colors[2])
+
+let list = [10, 'algo', true]
+console.log(list)
+
+let list2 = ['blabla', ['xx', 'yy']]
+console.log(list2[1][0])
+
