1e862dace0c1641004f41f8f8eea03151ddcd54e
André Freitas <andre.rfreitass@gmail.com>
2023-03-12 11:06:09 -0300

    arrow function

---
+++ B7/05-Funções/5-arrow-function/script.js
@@ -0,0 +1,16 @@
+// function somar (x, y) {
+//     return x + y
+// }
+
+// const somar = (x, y) => {
+//     return x + y
+// }
+
+const somar = (x, y) => x + y
+
+console.log(somar(10, 5))
+
+
+const sobrenome = sob => 'Bonieky ' + sob
+
+console.log(sobrenome('Lacerda'))
