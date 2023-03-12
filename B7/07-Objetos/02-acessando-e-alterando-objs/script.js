24d53d71c922867459ae63f95cf29cff6ba7e1c4
André Freitas <andre.rfreitass@gmail.com>
2023-03-12 12:00:05 -0300

    acessando e alterando objetos

---
+++ B7/07-Objetos/02-acessando-e-alterando-objs/script.js
@@ -0,0 +1,31 @@
+// let personagem = {
+//     nome: 'André',
+//     idade: 22,
+//     pais: 'Brasil',
+//     olhos: ['preto', 'azul'],
+//     caracteristicas: {
+//         forca: 20,
+//         magia: 5,
+//         stamina: 15
+//     }
+// }
+
+// personagem.nome = 'Pedro'
+// console.log(personagem.nome)
+
+// personagem.caracteristicas.forca += 5
+// console.log(personagem.caracteristicas.forca)
+
+// personagem.olhos.push('verde')
+// console.log(personagem.olhos)
+
+let personagem = {
+    nome: 'André',
+    idade: 22,
+    carros: [
+        {modelo: 'Fiat', cor: 'preto'},
+        {modelo: 'Ferrari', cor: 'vermelho'}
+    ]
+    }
+
+    console.log(personagem.carros[0].cor)
