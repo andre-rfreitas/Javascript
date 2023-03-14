938c4caa79577f7bc829af4aa7dac1e98f764d51
André Freitas <andre.rfreitass@gmail.com>
2023-03-14 19:18:46 -0300

    exercicios loops

---
+++ B7/09-Loop-While/exercicios/exerc.js
@@ -0,0 +1,14 @@
+// 1. Faça um loop que mostre todas as frutas
+let fruits = ['Maçã', 'Uva', 'Banana']
+
+for(let i = 0; i < fruits.length; i++)
+
+console.log(fruits[i])
+
+//2. Conte de 1 até 100 através de um WHILE
+let n = 1
+
+while (n <= 100) {
+    console.log(n)
+    n++
+}
