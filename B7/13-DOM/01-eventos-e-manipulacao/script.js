28b521b03f7c90ba4ae708a71b1b749ed29af819
André Freitas <andre.rfreitass@gmail.com>
2023-03-17 09:45:46 -0300

    DOM

---
+++ B7/13-DOM/01-eventos-e-manipulacao/script.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("h1")
+document.getElementById("teste")
+document.getElementsByClassName("botao")
+document.querySelector("#teste")
+document.querySelectorAll("#teste ul li")
