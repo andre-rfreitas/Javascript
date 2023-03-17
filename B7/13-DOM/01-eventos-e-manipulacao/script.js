56a70c6789e422a8e75c0cf1f253cec2c59697ab
André Freitas <andre.rfreitass@gmail.com>
2023-03-17 09:51:04 -0300

    eventos de clique

--- B7/13-DOM/01-eventos-e-manipulacao/script.js
+++ B7/13-DOM/01-eventos-e-manipulacao/script.js
@@ -1,5 +1,14 @@
+/*
 document.getElementsByTagName("h1")
 document.getElementById("teste")
 document.getElementsByClassName("botao")
 document.querySelector("#teste")
 document.querySelectorAll("#teste ul li")
+*/
+
+function clicou() {
+    alert("Clicou no botão")
+}
+
+let botao = document.querySelector(".botao")
+botao.addEventListener("click", clicou)
